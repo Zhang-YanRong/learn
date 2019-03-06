3ce4405b8066815dbd7e2c178dff47c0511ba6b1
zhangyanrong <479496771@qq.com>
2019-03-06 14:24:38 +0800

    pixi

---
+++ pixi/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+
+app.use(express.static("./public"));
+
+app.get('/', function (req, res) {
+    let fileName = path.resolve('./public/index.html');
+    res.sendFile(fileName)
+})
+
+app.get('/2.1', function (req, res) {
+    let fileName = path.resolve('./public/2.1.html');
+    res.sendFile(fileName)
+})
+
+app.listen(3000)
---
+++ pixi/public/b.js
@@ -0,0 +1,4 @@
+import {
+    add
+} from './a.js'
+console.log(add())
